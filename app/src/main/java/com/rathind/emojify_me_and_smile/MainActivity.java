8fc1ed245736cf3d889ac6669e2f7846d3924331
Rathi <rathind14@gmail.com>
2018-07-27 10:38:33 -0400

    initial commit

---
+++ app/src/main/java/com/rathind/emojify_me_and_smile/MainActivity.java
@@ -0,0 +1,13 @@
+package com.rathind.emojify_me_and_smile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
